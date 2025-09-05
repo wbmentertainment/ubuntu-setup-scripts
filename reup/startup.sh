b8ced51448167dbe846ffa63fddd49ba04fd0723
dev-binhnx <124752273+dev-binhnx@users.noreply.github.com>
2025-09-05 16:08:10 +0700

    Update startup.sh

--- reup/startup.sh
+++ reup/startup.sh
@@ -9,8 +9,6 @@ rm -rf NAS
 mkdir -p NAS
 mount -t cifs -o username=admin1,password=Came2020,vers=3.0,rw,iocharset=utf8,uid=1001,gid=1001,dir_mode=0777,file_mode=0777,noperm //192.168.1.111/media-reup NAS
 
-chmod -R 777 NAS/database
-
 echo "$GITHUB_TOKEN" | docker login ghcr.io -u "$GITHUB_USER" --password-stdin
 docker compose up -d
 docker image prune -f
