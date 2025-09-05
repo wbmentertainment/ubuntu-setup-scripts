00c78b80be3c0bd82f302cb642979572a1cb9704
dev-binhnx <124752273+dev-binhnx@users.noreply.github.com>
2025-09-05 10:56:06 +0700

    Update setup-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -11,8 +11,8 @@ MAC=$(cat /sys/class/net/$IFACE/address)
 # Đặt IP tĩnh (có thể sửa lại cho phù hợp mạng bạn)
 IPADDR="192.168.1.6/24"
 GATEWAY="192.168.1.1"
-DNS1="8.8.8.8"
-DNS2="1.1.1.1"
+DNS1="123.23.23.23"
+DNS2="123.26.26.26"
 
 # Backup file cloud-init nếu có
 if [ -f /etc/netplan/50-cloud-init.yaml ]; then
