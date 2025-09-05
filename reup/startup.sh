cb12f1412dec97e142f977b7bfb276d594a30cf6
dev-binhnx <124752273+dev-binhnx@users.noreply.github.com>
2025-09-05 15:56:10 +0700

    Update startup.sh

--- reup/startup.sh
+++ reup/startup.sh
@@ -5,13 +5,9 @@ cd /home/wbm/projects/media-reup
 docker compose down
 
 umount -a -t cifs -l || true
-
-rm -rf /home/wbm/projects/media-reup/NAS
-mkdir /home/wbm/projects/media-reup/NAS
-
-mount -t cifs -o username=admin1,password=Came2020,rw //192.168.1.111/media-reup /home/wbm/projects/media-reup/NAS
-
-chmod -R 777 /home/wbm/projects/media-reup/NAS
+rm -rf NAS
+mkdir -p NAS
+mount -t cifs -o username=admin1,password=Came2020,vers=3.0,iocharset=utf8,uid=1001,gid=1001,dir_mode=0777,file_mode=0777,noperm //192.168.1.111/media-reup NAS
 
 echo "$GITHUB_TOKEN" | docker login ghcr.io -u "$GITHUB_USER" --password-stdin
 docker compose up -d
